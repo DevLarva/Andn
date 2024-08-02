60aab9bc20d73b97627f20801a7ccce2ddd9f2c2
백대홍 <bdh3620@naver.com>
2024-08-03 01:15:20 +0900

    Add: 파일 이식작업(login)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import AndnMain from './Main/AndnMain';
+import ClientMain from './Main/ClientMain';
+import OutsourcingMain from './Main/OutsourcingMain';
+import { GlobalContextProvider } from './GlobalContext';
+import './styles.css';
+import PrivateRoute from './PrivateRoute'; // Import PrivateRoute
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/AndnMain"
+            element={
+              <PrivateRoute>
+                <AndnMain />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/ClientMain"
+            element={
+              <PrivateRoute>
+                <ClientMain />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/OutsourcingMain"
+            element={
+              <PrivateRoute>
+                <OutsourcingMain />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </GlobalContextProvider>
   );
 }
 
 export default App;
+
+
+/* TODO:
+- 로그인 여부에 따라 제한 접근 라우팅 구현 필요
+- 로그인 이후에 분기 처리 필요
+
+*/
